91611eceb904d8282710256422d70a63ba40f2bb
windyinmay <quynhanhmay.butter@gmail.com>
2022-11-06 13:36:18 +0700

    ex 2.11

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -1,19 +1,25 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react';
+import axios from 'axios';
 import PhoneBookForm from "./components/PhoneBookForm";
 import Filter from "./components/Filter";
 import Persons from "./components/Persons";
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-    ])
+    const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newPhone, setNewPhone] = useState('')
     const [filter, setFilter] = useState('')
 
+    useEffect(() => {
+        console.log('effect')
+        axios
+            .get('http://localhost:3001/persons')
+            .then(res => {
+                console.log('promis fulfilled')
+                setPersons(res.data)
+            })
+
+    },[])
     const handleFormNameChange = (e) => {
         // console.log(e.target.value)
         setNewName(e.target.value);
@@ -41,7 +47,7 @@ const App = () => {
             setNewPhone('');
         }
     }
-    console.log(filterNames)
+    // console.log(filterNames)
     return (
         <div>
             <h2>Phonebook</h2>
@@ -56,7 +62,6 @@ const App = () => {
             />
             <h3>Numbers</h3>
             <Persons persons={filterNames}/>
-            {/*{persons.map((p,index) => <li key={index}>{p.name}  {p.number}</li>)}*/}
         </div>
     )
 }
