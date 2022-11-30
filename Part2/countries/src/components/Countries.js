cc2e523f129e3563b76ec6cea220fe405e254e65
windyinmay <quynhanhmay.butter@gmail.com>
2022-12-01 02:19:58 +0700

    exercise 2.14 done

--- Part2/countries/src/components/Countries.js
+++ Part2/countries/src/components/Countries.js
@@ -4,8 +4,8 @@ import React from 'react';
 export default function Countries ({countriesFilter, showCountry}) {
     return(
         <div>
-            {countriesFilter.map(c=>
-                <li key={c.name.offical}>{c.name.common}
+            {countriesFilter.map((c, index)=>
+                <li key={index}>{c.name.common}
                     <button onClick ={() => showCountry(c.name.common)}>show</button></li>)}
         </div>
     )
--- Part2/countries/src/components/FoundCountry.js
+++ Part2/countries/src/components/FoundCountry.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
 export default function FoundCountry({countriesFilter}) {
     const languages = countriesFilter.map(c => c.languages);
-    // const flag = countriesFilter.map(c => c.flags.png)
-    // console.log(Object.values(languages[0]))
+    const api_key = process.env.REACT_APP_API_KEY;
+    const [weather, setWeather] = useState(null);
+    const URLBase = 'https://api.openweathermap.org/data/2.5/weather?';
+    const lat = countriesFilter.map(c => c.capitalInfo.latlng[0])
+    const lon = countriesFilter.map(c => c.capitalInfo.latlng[1])
+    useEffect(() => {
+        axios
+            .get(`${URLBase}lat=${lat}&lon=${lon}&appid=${api_key}`)
+            .then(res => setWeather(res.data))
+
+    }, [countriesFilter])
+
+    const tempInCelcius = weather && (Number(weather.main.temp) - 273.15).toFixed(2)
+    const iconCode = weather && weather.weather[0].icon
+    const icon = `http://openweathermap.org/img/wn/${iconCode}@2x.png`
+    console.log(iconCode)
     return(
         <div>
             <br/>
@@ -27,6 +42,12 @@ export default function FoundCountry({countriesFilter}) {
 
 
                 <h2>Weather in  {countriesFilter.map(c => c.capital)}</h2>
+
+                <p>temperature {tempInCelcius} Celcius</p>
+
+                <img src = {icon}  alt = 'Captial weather' width={150}/>
+
+                {weather && <p>wind {weather.wind.speed} m/s</p>}
             </div>
         </div>
     )
