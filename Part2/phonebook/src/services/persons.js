0fcecbc749bd0159365f0d7dba11ea9e2f666ebd
windyinmay <quynhanhmay.butter@gmail.com>
2023-03-21 00:07:57 +0200

    exercise 3.9 done

--- Part2/phonebook/src/services/persons.js
+++ Part2/phonebook/src/services/persons.js
@@ -1,29 +1,29 @@
-import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+import axios from "axios";
+const baseUrl = "http://localhost:3001/api/persons";
 
 // const baseUrl = '/api/persons';
 
 const getAll = () => {
-    const request = axios.get(baseUrl)
-    const nonExisting = {
-        id: 10000,
-        name: 'this person is not saved to the server',
-        number: '00000'
-        }  
-    return request.then(res => res.data.concat(nonExisting))
-}
+  const request = axios.get(baseUrl);
+  const nonExisting = {
+    id: 10000,
+    name: "this person is not saved to the server",
+    number: "00000",
+  };
+  return request.then((res) => res.data.concat(nonExisting));
+};
 
-const create = newObject => {
-    const resquest = axios.post(baseUrl, newObject)
-    return resquest.then(res => res.data)
-}
+const create = (newObject) => {
+  const resquest = axios.post(baseUrl, newObject);
+  return resquest.then((res) => res.data);
+};
 
 const update = (id, newObject) => {
-   const request = axios.put(`${baseUrl}/${id}`, newObject)
-    return request.then(res => res.data)
-}
+  const request = axios.put(`${baseUrl}/${id}`, newObject);
+  return request.then((res) => res.data);
+};
 const deletePerson = (id) => {
-    return axios.delete(`${baseUrl}/${id}`)
-}
+  return axios.delete(`${baseUrl}/${id}`);
+};
 
-export default { getAll, create, update, deletePerson }
+export default { getAll, create, update, deletePerson };
