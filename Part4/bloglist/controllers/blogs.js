e80aadf2309b6297150b4f094190f0f9569cd511
windyinmay <quynhanhmay.butter@gmail.com>
2023-03-30 00:44:26 +0300

    done ex 4.1

--- Part4/bloglist/controllers/blogs.js
+++ Part4/bloglist/controllers/blogs.js
@@ -1,39 +1,45 @@
-const blogsRouter = require('express').Router()
-const Blog = require('../models/blog')
+const blogsRouter = require("express").Router();
+const Blog = require("../models/blog");
 
-blogsRouter.get('/', (request, response) => {
-    Blog.find({}).then(blogs => {
-      response.json(blogs)
+blogsRouter.get("/", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+blogsRouter.get("/:id", (request, response, next) => {
+  Blog.findById(request.params.id)
+    .then((blog) => {
+      if (blog) {
+        response.json(blog);
+      } else {
+        response.status(404).end();
+      }
     })
-  })
-  
-blogsRouter.get('/:id', (request, response, next) => {
-    Blog.findById(request.params.id)
-      .then(blog => {
-        if (blog) {
-          response.json(blog)
-        } else {
-          response.status(404).end()
-        }
-      })
-      .catch(error => next(error))
-  })
+    .catch((error) => next(error));
+});
 
-blogsRouter.post('/', (request, response, next) => {
-    const {title, author, url, likes} = request.body
-    const blogObj = new Blog({
-      title: title,
-      author: author,
-      url: url,
-      likes: likes
+blogsRouter.post("/", (request, response, next) => {
+  const { title, author, url, likes } = request.body;
+  const blogObj = new Blog({
+    title: title,
+    author: author,
+    url: url,
+    likes: likes,
+  });
+  Blog.find({ title: title }).then((blog) => {
+    if (blog.length > 0) {
+      response.status(400).json({
+        error: `Blog's title already exists`,
+      });
+    }
+  });
+  blogObj
+    .save()
+    .then((result) => {
+      response.status(201).json(result);
     })
+    .catch((error) => next(error));
+});
 
-    blogObj
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
-      .catch(error => next(error))
-  })
-  
-  module.exports = blogsRouter
+module.exports = blogsRouter;
