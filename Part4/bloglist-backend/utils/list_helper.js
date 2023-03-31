f0449f088d214e147877a3192f6a1782e0bfe310
windyinmay <quynhanhmay.butter@gmail.com>
2023-03-31 16:54:44 +0300

    passed all test with blog list likes - exercise 4.4

--- Part4/bloglist-backend/utils/list_helper.js
+++ Part4/bloglist-backend/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   if(blogs.length >= 0) return 1;
 };
 
+const totalLikes = (blogs) => {
+  let sum = 0;
+  blogs.length >= 0 && blogs.map((blog) => {
+    sum += blog.likes;
+  });
+  return sum;
+};
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 };
