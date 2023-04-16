26c92aa83bd4a6fc2cce7b740f6512d18fca1776
windyinmay <quynhanhmay.butter@gmail.com>
2023-04-16 17:20:10 +0300

    router did not work since lack rootdir and baseurl in tsconfig

--- Part9/patientor/build/index.js
+++ Part9/patientor/build/index.js
@@ -4,13 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+// import patientRouter from './routes/patients';
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-const PORT = 3000;
-app.get('/api/ping', (_req, res) => {
+app.use((0, cors_1.default)());
+app.get('/ping', (_req, res) => {
     console.log('someone pinged here');
     res.send('pong');
 });
+// app.use('/api/patients', patientRouter);
+const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
--- Part9/patientor/src/index.ts
+++ Part9/patientor/src/index.ts
@@ -1,17 +1,17 @@
 import express from 'express';
 import cors from 'cors';
+import patientRouter from './routes/patients';
 
 const app = express();
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 
-const PORT = 3001;
-
-app.get('/api/ping', (_req, res) => {
+app.get('/ping', (_req, res) => {
 	console.log('someone pinged here');
 	res.send('pong');
 });
-
+app.use('/api/patients', patientRouter);
+const PORT = 3001;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
