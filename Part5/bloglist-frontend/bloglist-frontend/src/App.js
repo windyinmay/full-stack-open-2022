4da8c38934e7fa15ca819187b3fd2a5097e2647d
windyinmay <quynhanhmay.butter@gmail.com>
2023-04-26 00:07:20 +0300

    bloglist frontend own created instead of using example solution

---
+++ Part5/bloglist-frontend/bloglist-frontend/src/App.js
@@ -0,0 +1,138 @@
+import React, { useState, useEffect } from "react";
+import blogService from "./services/blogs";
+
+import Filter from "./components/Filter";
+import Notification from "./components/Notification";
+import Blogs from "./components/Blogs";
+import BlogForm from "./components/BlogForm";
+
+import Container from "@mui/material/Container";
+
+const App = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [newBlog, setNewBlog] = useState({
+    title: "",
+    author: "",
+    url: "",
+    likes: 0,
+  });
+  const [filter, setFilter] = useState("");
+  const [updateMsg, setUpdateMsg] = useState(null);
+
+  useEffect(() => {
+    console.log("effect");
+    blogService
+      .getAll()
+      .then((initialBlogs) => {
+        console.log("promis fulfilled");
+        setBlogs(initialBlogs);
+      })
+      .catch((err) => {
+        console.log("fail");
+      });
+  }, []);
+
+  const handleFormChange = (e) =>
+    setNewBlog({ ...newBlog, [e.target.name]: e.target.value });
+
+  const handleFilter = (e) => setFilter(e.target.value.toLowerCase());
+
+  const addBlog = (e) => {
+    e.preventDefault();
+    const existingBlogs = blogs.filter(
+      (blog) => blog.title.toLowerCase() === newBlog.title.trim().toLowerCase()
+    );
+
+    if (existingBlogs.length > 0) {
+      const confirmationMsg = window.confirm(
+        `${newBlog.title} is already added to the blog list, replace the old information with the new one?`
+      );
+
+      confirmationMsg &&
+        blogService
+          .update(existingBlogs[0].id, newBlog)
+          .then((updatedBlog) => {
+            setBlogs(
+              blogs.map((blog) =>
+                blog.id === updatedBlog.id ? updatedBlog : blog
+              )
+            );
+            setNewBlog({ title: "", author: "", url: "", likes: 0 });
+            setUpdateMsg(`Successfully updated blog ${updatedBlog.title}`);
+            setTimeout(() => {
+              setUpdateMsg(null);
+            }, 5000);
+            setBlogs(blogs.filter((blog) => blog.id !== updatedBlog.id));
+          })
+          .catch((error) => {
+            setUpdateMsg("failed");
+            setTimeout(() => setUpdateMsg(null), 5000);
+          });
+    } else {
+      blogService
+        .create(newBlog)
+        .then((returnedBlog) => {
+          setBlogs(blogs.concat(returnedBlog));
+          setNewBlog({ title: "", author: "", url: "", likes: 0 });
+          setUpdateMsg(`Added ${returnedBlog.title}`);
+          setTimeout(() => setUpdateMsg(null), 5000);
+        })
+        .catch((error) => {
+          setUpdateMsg(error.response.data.error);
+          setTimeout(() => setUpdateMsg(null), 5000);
+          console.log(error.response.data.error);
+        });
+    }
+  };
+
+  const deleteBlog = (blog) => {
+    const { id, title } = blog;
+    const confirmationMsg = window.confirm(`Delete the blog ${title}?`);
+
+    confirmationMsg &&
+      blogService
+        .deleteBlog(id)
+        .then(() => {
+          const updatedBlog = blogs.filter((blog) => blog.id !== id);
+          setUpdateMsg(`${blog.title} has been removed.`);
+          setTimeout(() => setUpdateMsg(null), 5000);
+          setBlogs(updatedBlog);
+        })
+        .catch((error) => {
+          setUpdateMsg(
+            `The blog with the title '${blog.title}' was removed from the server`
+          );
+          setTimeout(() => setUpdateMsg(null), 5000);
+          setBlogs(blogs.filter((blog) => blog.id !== id));
+        });
+  };
+  const filterTitles =
+    blogs.length > 0 &&
+    blogs.filter((blog) => blog.title.toLowerCase().includes(filter));
+
+  return (
+    <Container
+      sx={{
+        marginLeft: 2,
+        width: "auto",
+        height: "100vh",
+      }}
+    >
+      <h1>Blog list</h1>
+      <Notification message={updateMsg} />
+      <Filter handleFilter={handleFilter} />
+      <h2>Add a new blog</h2>
+      <BlogForm
+        newBlog={newBlog}
+        handleFormChange={handleFormChange}
+        addBlog={addBlog}
+      />
+      <h2>List of the blogs</h2>
+      {filterTitles.length > 0 && (
+        <Blogs blogs={filterTitles} deleteBlog={deleteBlog} />
+      )}
+    </Container>
+  );
+};
+
+export default App;
---
+++ Part5/bloglist-frontend/bloglist-frontend/src/components/BlogForm.js
@@ -0,0 +1,102 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import FormControl from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
+import { Button } from "@mui/material";
+
+export default function BlogForm({ addBlog, newBlog, handleFormChange }) {
+  return (
+    // <div>
+    //   <form onSubmit={addBlog}>
+    //     <div>
+    //       Title:
+    //       <input
+    //         name="title"
+    //         type="text"
+    //         value={newBlog.title}
+    //         onChange={handleFormChange}
+    //       />
+    //     </div>
+    //     <div>
+    //       Author:
+    //       <input
+    //         name="author"
+    //         type="text"
+    //         value={newBlog.author}
+    //         onChange={handleFormChange}
+    //       />
+    //     </div>
+    //     <div>
+    //       URL:
+    //       <input
+    //         name="url"
+    //         type="text"
+    //         value={newBlog.url}
+    //         onChange={handleFormChange}
+    //       />
+    //     </div>
+    //     <div>
+    //       Likes:
+    //       <input
+    //         name="likes"
+    //         type="number"
+    //         value={newBlog.likes}
+    //         onChange={handleFormChange}
+    //       />
+    //     </div>
+    //     <div>
+    //       <button type="submit">add</button>
+    //     </div>
+    //   </form>
+    // </div>
+    <Box
+      component="form"
+      sx={{
+        "& > :not(style)": { m: 1 },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <FormControl>
+        <TextField
+          sx={{ marginBottom: 2 }}
+          name="title"
+          type="text"
+          label="Title"
+          value={newBlog.title}
+          onChange={handleFormChange}
+        />
+
+        <TextField
+          sx={{ marginBottom: 2 }}
+          name="author"
+          type="text"
+          label="Author"
+          value={newBlog.author}
+          onChange={handleFormChange}
+        />
+
+        <TextField
+          sx={{ marginBottom: 2 }}
+          name="url"
+          type="url"
+          label="URL"
+          value={newBlog.url}
+          onChange={handleFormChange}
+        />
+
+        <TextField
+          sx={{ marginBottom: 2 }}
+          name="likes"
+          label="Likes"
+          type="number"
+          value={newBlog.likes}
+          onChange={handleFormChange}
+        />
+        <Button variant="outlined" color="success" onClick={addBlog}>
+          Add New Blog
+        </Button>
+      </FormControl>
+    </Box>
+  );
+}
